0fcec706a5498ed44787239b14e19e8ebdb6af22
Bear Flinn <bearflinn@gmail.com>
2025-12-12 18:23:26 -0500

    filter local addresses

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -4,7 +4,7 @@ output "vms" {
     for name, vm in proxmox_virtual_environment_vm.debian_vm : name => {
       vmid         = vm.vm_id
       name         = vm.name
-      ip_addresses = vm.ipv4_addresses
+      ip_addresses = flatten([for addr_list in vm.ipv4_addresses : [for addr in addr_list : addr if !startswith(addr, "127.")]])
     }
   }
 }
